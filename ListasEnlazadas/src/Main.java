500c2dc7c226897c44799f278477784a01f2a442
Leonardo <162130975+Pableox21@users.noreply.github.com>
2025-05-02 10:14:26 -0400

    2/5/25

--- ListasEnlazadas/src/Main.java
+++ ListasEnlazadas/src/Main.java
@@ -1,8 +1,10 @@
 import Ise.ListaSimplementeEnlazada;
+import icse.ListaCircularSimplementeEnlazada;
+import ide.ListaDoblementeEnlazada;
 
 public class Main {
     public static void main(String[] args) {
-        ListaSimplementeEnlazada listaSimplementeEnlazada=new ListaSimplementeEnlazada();
+       /* ListaSimplementeEnlazada listaSimplementeEnlazada=new ListaSimplementeEnlazada();
         System.out.println(listaSimplementeEnlazada.estaVacia());
 
         //Implementar elementos a nuestra lista
@@ -22,6 +24,39 @@ public class Main {
 
         //mostramos nuestra lista
         listaSimplementeEnlazada.mostar();
+        */
+        /*ListaDoblementeEnlazada listaDoblementeEnlazada=new ListaDoblementeEnlazada();
+        System.out.println(listaDoblementeEnlazada.estaVacio());
 
+        for (int i=20;i> 0;i--){
+            listaDoblementeEnlazada.Insertar(i);
+        }
+
+        System.out.println(listaDoblementeEnlazada.estaVacio());
+        listaDoblementeEnlazada.mostrarAdelante();
+
+        listaDoblementeEnlazada.eliminar();
+        listaDoblementeEnlazada.eliminar();
+
+        listaDoblementeEnlazada.mostrarAdelante();
+        listaDoblementeEnlazada.mostrarAtras();
+         */
+        ListaCircularSimplementeEnlazada listaCircularSimplementeEnlazada=new ListaCircularSimplementeEnlazada();
+        System.out.println(listaCircularSimplementeEnlazada.estaVacio());
+        System.out.println(listaCircularSimplementeEnlazada.tamaño());
+
+        for (int i=1;i<=12;i++){
+            listaCircularSimplementeEnlazada.insertarFin(i);
+        }
+
+        listaCircularSimplementeEnlazada.mostrar();
+        System.out.println(listaCircularSimplementeEnlazada.tamaño());
+
+        //listaCircularSimplementeEnlazada.eliminarFin();
+        //listaCircularSimplementeEnlazada.eliminarFin();
+
+        //listaCircularSimplementeEnlazada.mostrar();
+
+        listaCircularSimplementeEnlazada.mostrarCircular(2);
     }
 }
---
+++ ListasEnlazadas/src/icse/ListaCircularSimplementeEnlazada.java
@@ -0,0 +1,86 @@
+package icse;
+
+public class ListaCircularSimplementeEnlazada {
+    private Nodo inicio;
+
+    public ListaCircularSimplementeEnlazada(){
+        inicio=null;
+    }
+
+    public boolean estaVacio(){
+        return inicio==null;
+    }
+    public int tamaño(){
+        int contador=0;
+
+        if (!estaVacio()){
+            Nodo ultimo=inicio;
+            while (ultimo.getSiguiente()!=inicio){
+                ultimo=ultimo.getSiguiente();
+
+                contador++;
+            }
+            contador++;
+        }
+
+        return contador;
+    }
+
+    public void insertarFin(Object dato){
+        if (estaVacio()){
+            Nodo nuevo=new Nodo();
+            nuevo.setDato(dato);
+            nuevo.setSiguiente(nuevo);
+
+            inicio=nuevo;
+        }else {
+            Nodo nuevo=new Nodo();
+            nuevo.setDato(dato);
+            nuevo.setSiguiente(inicio);
+
+            Nodo ultimo=inicio;
+            while (ultimo.getSiguiente()!=inicio){
+                ultimo=ultimo.getSiguiente();
+            }
+            ultimo.setSiguiente(nuevo);
+        }
+    }
+
+    public void eliminarFin(){
+        if (!estaVacio()){
+            Nodo penultimo=inicio;
+            while (penultimo.getSiguiente().getSiguiente()!=inicio){
+                penultimo=penultimo.getSiguiente();
+            }
+            penultimo.setSiguiente(inicio);
+        }
+    }
+    public void mostrar(){
+        if (!estaVacio()){
+            Nodo ultimo=inicio;
+            while (ultimo.getSiguiente()!=inicio){
+                System.out.print(ultimo.getDato()+"     ");
+
+                ultimo=ultimo.getSiguiente();
+            }
+            System.out.print(ultimo.getDato()+"   ");
+            System.out.println();
+        }
+    }
+
+    public void mostrarCircular(int vueltas){
+        if (!estaVacio()){
+            int contador=0;
+
+            Nodo temp=inicio;
+            while (temp!=null && contador<(tamaño()*vueltas)){
+                System.out.print(temp.getDato()+"   ");
+
+                temp=temp.getSiguiente();
+
+                contador++;
+            }
+            System.out.println();
+        }
+    }
+}
