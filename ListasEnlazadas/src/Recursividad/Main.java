3ba5544b59f41d8ba7338abdd51516cdd3a1a027
pableox21 <leonax234@gmail.com>
2025-05-16 09:01:45 -0400

    16/05/25

---
+++ ListasEnlazadas/src/Recursividad/Main.java
@@ -0,0 +1,8 @@
+package Recursividad;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println(Ejercicios.factorial(5));
+        System.out.println(Ejercicios.mcd(412,184));
+    }
+}
---
+++ ListasEnlazadas/src/Recursividad/MainMCD.java
@@ -0,0 +1,4 @@
+package Recursividad;
+
+public class MainMCD {
+}
